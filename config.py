d90c786d4bbf79da1927a5e49866bf8188e060eb
amitbrown21 <120206394+amitbrown21@users.noreply.github.com>
2024-02-24 18:34:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 # Enter your discord token here
-TOKEN = 'MTIwNzA5MzQxMjg4NTU2NTUxMA.G0MEcJ.5FDEVYPewT9s4etbiU_L2HBbBivuwgwzRgB7jg'
+TOKEN = 'Enter your discord token here'
 # enter a test id for your server
-main_channel_id = 792482176485687326
-test_channel_id = 1207106336614121483
+main_channel_id = 'can leave non for all channels'
+test_channel_id = 'Channel ID'
 # enter a prefix for the bot (if you dont want to use slash commands)
 prefix = '/'
